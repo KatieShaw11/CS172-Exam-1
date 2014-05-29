66572661a8e98fa3266384dce1597240361c812c
KatieShaw11 <Katie S>
2014-05-28 22:50:39 -0700

    prep work for exam 1

---
+++ Exam 1/Exam 1/main.cpp
@@ -0,0 +1,18 @@
+/////////////////
+//  Katie Shaw
+//  Exam 1
+//  CS172
+//  Pete Tucker
+//  5/29/2014
+/////////////////
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+}
+
